38d38561d6d35cc7897e2cdd3f6abdf094022cbf
Marie soleil Jarry <mjarry@c3r6p6.42quebec.com>
2023-03-27 17:18:52 -0400

    compiles with texture merge

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marie-soleiljarry <marie-soleiljarry@st    +#+  +:+       +#+        */
+/*   By: mjarry <mjarry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 16:29:39 by gvial             #+#    #+#             */
-/*   Updated: 2023/02/22 15:06:12 by marie-solei      ###   ########.fr       */
+/*   Updated: 2023/03/27 17:18:28 by mjarry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ static void	init(t_cub3d *cub3d, char *map_path)
 	cub3d->map.map_c = NULL;
 	cub3d->map.height = 0;
 	cub3d->map.width = 0;
-	// cub3d->display.mlx = mlx_init();
+	cub3d->display.mlx = mlx_init();
 	cub3d->display.mlx_win = NULL;
 	cub3d->display.img_width = 0;
 	cub3d->display.img_height = 0;
@@ -66,10 +66,10 @@ int	main(int argc, char **argv)
 		arg_error();
 	init(&cub3d, argv[1]);
 	parse(&cub3d);
-	// display_window(&cub3d);
-	// mlx_hook(cub3d.display.mlx_win, 2, 0, key_hook, &cub3d);
-	// mlx_hook(cub3d.display.mlx_win, 17, 0, close_x, &cub3d);
-	// mlx_loop(cub3d.display.mlx);
+	display_window(&cub3d);
+	mlx_hook(cub3d.display.mlx_win, 2, 0, key_hook, &cub3d);
+	mlx_hook(cub3d.display.mlx_win, 17, 0, close_x, &cub3d);
+	mlx_loop(cub3d.display.mlx);
 	free_textures(&cub3d);
 	free_map(&cub3d);
 }
